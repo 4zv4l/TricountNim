ebe3fb6a602f77c81c9349fa13612409b60e0591
azz <4zv4l@protonmail.com>
2023-03-08 22:32:44 +0100

    split into proc

--- pentacount.nim
+++ pentacount.nim
@@ -2,51 +2,65 @@ import std/[rdstdin,tables,strutils,strformat]
 import regex
 import tablelib
 
-var
-  filename = readLineFromStdin("filename: ")
-  file = open(filename)
-  get  = initTable[string, float]() 
-  give = initTable[string, float]()
-  sold = initTable[string, float]()
-  reg  = re"^(?:[^\s]+)\s(?P<src>.+)\spaye\s(?P<money>\d+(\.\d+)?)\sa\s(?:(?P<dst>[^\s]+)\s?)+$"
-  res: RegexMatch
+type Account = Table[string, float]
 
-# load give/get money
-for line in file.lines:
-  if match(line, reg, res):
-    var
-      src = res.group("src", line)[0]
-      money = parseFloat(res.group("money", line)[0])
-      dst = res.group("dst", line)
-    give[src] = give.getOrDefault(src) + money
-    for ppl in dst:
-      get[ppl] = get.getOrDefault(ppl) + money/float(dst.len)
-
-# difference between give/get to get the sold
-for ppl in get.keys:
-  sold[ppl] = get.getOrDefault(ppl) - give.getOrDefault(ppl)
-
-# show the sold, if negative means you have to get money
-# otherwise means you should give money to others
-echo "~ Here are the totals to send/give ~"
-echo ""
-for (ppl, money) in sold.pairs:
-  if money >= 0:
-    echo fmt"{ppl:<6} should give: {money:>7.2f}"
-  else:
-    echo fmt"{ppl:<6} should get : {abs(money):>7.2f}"
-echo ""
-echo "~ Here is the repartition ~"
-echo ""
-
-# process and show who should pay who
-while smallest(sold).val != 0:
+proc loadAccount(filename: string, reg: Regex): tuple[give, get: Account] =
+  ## read `filename` line by line using the given regex to get:
+  ## - `src` is who pay
+  ## - `money` is the amount
+  ## - `dst` is who get (can be multiple people)
+  # load the Accounts of each person
   var
-    higher = largest(sold)
-    lower = smallest(sold)
-    amount = min(-lower.val, higher.val)
-  echo fmt"{higher.key:<6} should pay {lower.key:<6}: {amount:>6.2f}"
-  sold[lower.key] += amount
-  sold[higher.key] -= amount
-  if sold[lower.key] == 0:
-    sold.del(lower.key)
+    file = open(filename)
+    res: RegexMatch
+  result = (initTable[string, float](), initTable[string, float]())
+  for line in file.lines:
+    if match(line, reg, res):
+      var
+        src = res.group("src", line)[0]
+        money = parseFloat(res.group("money", line)[0])
+        dst = res.group("dst", line)
+      result.give[src] = result.give.getOrDefault(src) + money
+      for ppl in dst:
+        result.get[ppl] = result.get.getOrDefault(ppl) + money/float(dst.len)
+
+proc getSold(give, get: Account): Account =
+  ## calcul the sold of each person (get - give)
+  result = initTable[string, float]()
+  for ppl in get.keys:
+    result[ppl] = get.getOrDefault(ppl) - give.getOrDefault(ppl)
+
+proc showSold(sold: Account) =
+  ## show the the amount each person should give or get
+  echo "~ Here are the totals to send/give ~"
+  echo ""
+  for (ppl, money) in sold.pairs:
+    if money >= 0:
+      echo fmt"{ppl:<6} should give: {money:>7.2f}"
+    else:
+      echo fmt"{ppl:<6} should get : {abs(money):>7.2f}"
+  echo ""
+  echo "~ Here is the repartition ~"
+  echo ""
+
+proc processSold(sold: var Account) =
+  ## process and show who should pay who and how much
+  while smallest(sold).val != 0:
+    var
+      higher = largest(sold)
+      lower = smallest(sold)
+      amount = min(-lower.val, higher.val)
+    echo fmt"{higher.key:<6} should pay {lower.key:<6}: {amount:>6.2f}"
+    sold[lower.key] += amount
+    sold[higher.key] -= amount
+    if sold[lower.key] == 0:
+      sold.del(lower.key)
+
+when isMainModule:
+  var
+    filename = readLineFromStdin("filename: ")
+    reg = re"^(?:[^\s]+)\s(?P<src>.+)\spaye\s(?P<money>\d+(\.\d+)?)\sa\s(?:(?P<dst>[^\s]+)\s?)+$"
+    (give, get) = loadAccount(filename, reg)
+    sold = getSold(give, get)
+  showSold(sold)
+  processSold(sold)
